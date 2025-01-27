3809b7a5958d1c948644a5ce6684c256e5d7a170
sihyeong <syhy7051@gmail.com>
2025-01-27 16:51:28 +0900

    add minsu api

---
+++ src/main/java/com/syhy/git_team_practice/controller/MinsuController.java
@@ -0,0 +1,19 @@
+package com.syhy.git_team_practice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.syhy.git_team_practice.dto.MemberResponseDTO;
+
+@RestController
+public class MinsuController {
+
+    @GetMapping("/minsu")
+    public MemberResponseDTO minsu() {
+        return MemberResponseDTO.builder()
+                .name("민수")
+                .email("minsu@gmail.com")
+                .age("25")
+                .build();
+    }
+}
