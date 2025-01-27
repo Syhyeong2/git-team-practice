7910ba7c46e6c05312feb85f4ca888989077e89d
6152km <kmg6152@gmail.com>
2025-01-27 18:29:06 +0900

    first commit

---
+++ src/main/java/com/syhy/git_team_practice/controller/MyungGeunController.java
@@ -0,0 +1,19 @@
+package com.syhy.git_team_practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.syhy.git_team_practice.dto.MemberResponseDTO;
+
+@Controller
+public class MyungGeunController {
+
+    @GetMapping("/myunggeun")
+    public MemberResponseDTO myunggeun() {
+        return MemberResponseDTO.builder()
+                .name("명근")
+                .email("kmg6152@gmail.com")
+                .age("30")
+                .build();
+    }
+}
